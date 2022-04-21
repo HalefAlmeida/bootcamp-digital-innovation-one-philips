1a1e74c24c4db69a78895a7451fa3478ce96b9b6
halefalmeida <halef.dorigan@edu.unipar.br>
2022-04-21 15:29:23 -0300

    added desafio de código

---
+++ projetos/desafios-de-codigo/desafio-js-1.js
@@ -0,0 +1,10 @@
+const numTotal = parseInt(gets())
+const numFigCompradas = parseInt(gets())
+const setFig = new Set()
+
+for (figura of numFigCompradas) {
+  //   const fig = parseInt(gets())
+  //   setFig.add(fig)
+}
+
+print(numTotal - setFig.size)
