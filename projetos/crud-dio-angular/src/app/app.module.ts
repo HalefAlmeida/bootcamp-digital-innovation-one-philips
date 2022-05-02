5a8a6b33fc307ec166ba027ccf083045720f098c
halefalmeida <halef.dorigan@edu.unipar.br>
2022-05-02 20:45:52 -0300

    added crud base application

---
+++ projetos/crud-dio-angular/src/app/app.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { UsersListComponent } from './pages/users/users-list/users-list.component';
+
+@NgModule({
+  declarations: [AppComponent, HeaderComponent, UsersListComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
